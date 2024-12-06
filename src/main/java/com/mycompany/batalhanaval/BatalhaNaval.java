c44a65936465804cf4a6096bcbec8440fa2d2429
JersonVitor <jersonvitor2016@gmail.com>
2024-12-05 21:03:03 -0300

    first commit

---
+++ src/main/java/com/mycompany/batalhanaval/BatalhaNaval.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.batalhanaval;
+
+/**
+ *
+ * @author usuario
+ */
+public class BatalhaNaval {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TelaPrincipal().setVisible(true);
+            }
+        });
+       
+    }
+}
