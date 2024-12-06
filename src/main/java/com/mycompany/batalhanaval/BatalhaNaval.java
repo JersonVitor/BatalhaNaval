6765c8d8f9e043078b36f07a717417e14a262f55
JersonVitor <jersonvitor2016@gmail.com>
2024-12-05 21:05:06 -0300

    upload

--- src/main/java/com/mycompany/batalhanaval/BatalhaNaval.java
+++ src/main/java/com/mycompany/batalhanaval/BatalhaNaval.java
@@ -20,3 +20,63 @@ public class BatalhaNaval {
        
     }
 }
+/*
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Collections;
+
+import static javax.swing.GroupLayout.Alignment.LEADING;
+
+public class Tela extends JFrame {
+
+    JPanel mainPanel;
+    public Tela(){
+        initComponents();
+        addMatrizes();
+    }
+
+    private void addMatrizes() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(2, 1, 10, 10)); // 1 linha, 2 colunas, espaçamento de 10px
+        JPanel p1 =addMatrix();
+        JPanel p2 = addMatrix();
+        mainPanel.add(p1);
+        mainPanel.add(p2);
+        this.getContentPane().add(mainPanel, BorderLayout.WEST);
+    }
+
+    private JPanel addMatrix() {
+        JPanel jp = new JPanel(new GridLayout(8,8));
+        ButtonGroup bg = new ButtonGroup();
+        for (int i = 0; i < 8;i++){
+            for (int j = 0; j<8; j++){
+                JToggleButton b = new JToggleButton();
+                b.setFocusable(false);
+                b.addActionListener(e -> {
+                    b.setBackground(b.isSelected() ? Color.GREEN: Color.GRAY);
+                    for (AbstractButton btn: Collections.list(bg.getElements())){
+                        if(btn!= b){
+                            btn.setSelected(false);
+                            btn.setBackground(Color.gray);
+                        }
+                    }
+                });
+
+                bg.add(b);
+                jp.add(b);
+            }
+        }
+        return jp;
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 400); // Define o tamanho da janela
+        setLocationRelativeTo(null); // Centraliza a janela na tela
+        setLayout(new BorderLayout());
+    }
+
+}
+
+*/
--- src/main/java/com/mycompany/batalhanaval/Conexão.java
+++ src/main/java/com/mycompany/batalhanaval/Conexão.java
@@ -26,21 +26,92 @@ public class Conexão extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Conectar");
+
+        jTextField1.setText("jTextField1");
+
+        jLabel2.setText("IP Cliente");
+
+        jLabel3.setText("IP Cliente");
+
+        jTextField2.setText("jTextField1");
+
+        jButton1.setLabel("Salvar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 10, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jButton1)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel2))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(12, 12, 12))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(31, 31, 31)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 141, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(21, 21, 21))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +149,12 @@ public class Conexão extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
